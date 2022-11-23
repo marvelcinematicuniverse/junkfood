724eb37516d55e94055f7aebe4e7cf571ad63ab3
marvelcinematicuniverse <118898122+marvelcinematicuniverse@users.noreply.github.com>
2022-11-23 03:50:53 -0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
-linux is afree open source
+linux is a free open source
 linux is free oerating system
 linux is multiple oerating system
 unix is not GNU
+Multiple source of operating system
+naddy is a good and funny guy
+best freind is everything
+
